67bfac2b84fb051d0a4595beb02803b207d1708a
Darshan Tonape <tonapedarshan1@gmail.com>
2025-12-18 14:58:13 +0530

    Implement iterative binary search in Java
    This program implements a binary search algorithm using an iterative approach. It prompts the user for input, searches for the specified element, and displays the result.

--- Searching/BinarySearchIterative.java
+++ Searching/BinarySearchIterative.java
@@ -1,1 +1,60 @@
+//Write a Java program to implement Binary Search using Iterative method.
+
+import java.util.Scanner;
+
+public class BinarySearchIterative {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, key;
+        int[] arr;
+
+        System.out.print("Enter number of elements: ");
+        n = sc.nextInt();
+
+        arr = new int[n];
+
+        System.out.println("Enter elements in sorted order:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to search: ");
+        key = sc.nextInt();
+
+        int low = 0;
+        int high = n - 1;
+        int mid;
+        int flag = 0;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println("Element found at position: " + (mid + 1));
+                flag = 1;
+                break;
+            } else if (arr[mid] < key) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        if (flag == 0) {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
+
+//output:
+Enter number of elements: 5
+Enter elements in sorted order:
+10 20 30 40 50
+Enter element to search: 30
+  
+Element found at position: 3
 
