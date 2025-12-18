8907712559a716c9490adf822f61456359b2e119
Darshan Tonape <tonapedarshan1@gmail.com>
2025-12-18 15:13:20 +0530

    Add Insertion Sort implementation in Java
    Implemented Insertion Sort algorithm to sort an array of integers input by the user and display the sorted array.

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,1 +1,53 @@
+//Write a Java program to sort an array using Insertion Sort technique.
+
+import java.util.Scanner;
+
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        int[] arr;
+
+        System.out.print("Enter number of elements: ");
+        n = sc.nextInt();
+
+        arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // insertion sort logic
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        System.out.println("Sorted array:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
+
+//Output:
+Enter number of elements: 6
+Enter array elements:
+25 12 40 5 18 30
+
+Sorted array:
+5 12 18 25 30 40
 
